b0b40ac6b4b97f1cbd036fc90d90be9f0b89725a
davit dvalashvili <davitdvalashvili1996@gmail.com>
2024-11-21 20:22:09 +0400

    feat: update fitCounter modal

--- client/src/components/DevicesComponent/DeviceCardComponents/FitCounter.tsx
+++ client/src/components/DevicesComponent/DeviceCardComponents/FitCounter.tsx
@@ -1,133 +1,192 @@
-import axios from 'axios';
-import { useParking } from '../../../App';
-import { Modal, ModalBody, ModalContainer, ModalFooter, ModalHeader } from '../../Modal';
-import { FC, useEffect, useState } from 'react';
-import { Variant1 } from '../../../views/Main/Counter';
-import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
-import { FiCheck } from 'react-icons/fi';
+import axios from "axios";
+import { useParking } from "../../../App";
+import {
+  Modal,
+  ModalBody,
+  ModalContainer,
+  ModalFooter,
+  ModalHeader,
+} from "../../Modal";
+import { FC, useEffect, useState } from "react";
+import { Variant1 } from "../../../views/Main/Counter";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { FiCheck } from "react-icons/fi";
 
 type FitCounter = {
-	selectedDevice: Device;
-	setDevices: React.Dispatch<React.SetStateAction<Device[]>>;
-	hideFitDevice: () => void;
+  selectedDevice: Device;
+  setDevices: React.Dispatch<React.SetStateAction<Device[]>>;
+  hideFitDevice: () => void;
 };
 
-const FitCounter: FC<FitCounter> = ({ selectedDevice, setDevices, hideFitDevice }): JSX.Element => {
-	const { API_URL, showNotification } = useParking();
+const FitCounter: FC<FitCounter> = ({
+  selectedDevice,
+  setDevices,
+  hideFitDevice,
+}): JSX.Element => {
+  const { API_URL, showNotification } = useParking();
 
-	const [counters, setCounters] = useState<Variant1[]>([]);
-	const [selectedCounter, setSelectedCounter] = useState<number | null>(null);
-	const [selectedDropdown, setSelectedDropdown] = useState<number | null>(null);
+  const [counters, setCounters] = useState<Variant1[]>([]);
+  const [selectedCounter, setSelectedCounter] = useState<number | null>(null);
+  const [selectedDropdown, setSelectedDropdown] = useState<number | null>(null);
 
-	const getCounters = async () => {
-		await axios
-			.get(API_URL + '/getcounters')
-			.then((res) => {
-				if (res.status >= 200 && res.status <= 226) {
-					setCounters(res.data);
-				}
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
+  const getCounters = async () => {
+    await axios
+      .get(API_URL + "/getcounters")
+      .then((res) => {
+        if (res.status >= 200 && res.status <= 226) {
+          setCounters(res.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-	const fitCounter = async () => {
-		await axios.post(API_URL + '/fitcounter', { device_id: selectedDevice.device_id, counter_id: selectedCounter }).then((res) => {
-			if (res.status >= 200 && res.status <= 226) {
-				const { status, message } = res.data as ResponseStatus;
-				if (status === 'updated') {
-					setDevices((devices) => {
-						return devices.map((device) => {
-							if (device.device_id === selectedDevice.device_id) {
-								return {
-									...device,
-									counter_id: selectedCounter as number,
-								};
-							} else {
-								return device;
-							}
-						});
-					});
-					showNotification(message);
-					hideFitDevice();
-				}
-			}
-		});
-	};
+  const fitCounter = async () => {
+    await axios
+      .post(API_URL + "/fitcounter", {
+        device_id: selectedDevice.device_id,
+        counter_id: selectedCounter,
+      })
+      .then((res) => {
+        if (res.status >= 200 && res.status <= 226) {
+          const { status, message } = res.data as ResponseStatus;
+          if (status === "updated") {
+            setDevices((devices) => {
+              return devices.map((device) => {
+                if (device.device_id === selectedDevice.device_id) {
+                  return {
+                    ...device,
+                    counter_id: selectedCounter as number,
+                  };
+                } else {
+                  return device;
+                }
+              });
+            });
+            showNotification(message);
+            hideFitDevice();
+          }
+        }
+      });
+  };
 
-	const selectCounter = (id: number) => {
-		setSelectedCounter(id);
-	};
+  const selectCounter = (id: number) => {
+    setSelectedCounter(id);
+  };
 
-	const selectDropdown = (idx: number) => {
-		if (selectedDropdown === idx) setSelectedDropdown(-1);
-		else setSelectedDropdown(idx);
-	};
+  const selectDropdown = (idx: number) => {
+    if (selectedDropdown === idx) setSelectedDropdown(-1);
+    else setSelectedDropdown(idx);
+  };
 
-	useEffect(() => {
-		getCounters();
-		if (selectedDevice) setSelectedCounter(selectedDevice.counter_id);
-	}, []);
+  useEffect(() => {
+    getCounters();
+    if (selectedDevice) setSelectedCounter(selectedDevice.counter_id);
+  }, []);
 
-	return (
-		<ModalContainer>
-			<Modal>
-				<ModalHeader title='მრიცხველის მორგება' onHide={hideFitDevice} />
-				<ModalBody>
-					<div>
-						<div className='text-sm ps-3 italic text-gray-700'>{selectedDevice.device_name}</div>
-						{counters.map((counter, i) => {
-							return (
-								<div key={i} className='p-3 border mt-2 rounded-md'>
-									<div className='flex items-center justify-between'>
-										<div className='flex items-center gap-x-3'>
-											<div className='w-5 h-5 border cursor-pointer flex items-center justify-center' onClick={() => selectCounter(counter.counter_id as number)}>
-												{counter.counter_id === selectedCounter && <FiCheck />}
-											</div>
-											<span className='mt-0.5 text-gray-800'>{counter.counter_name}</span>
-										</div>
-										<span className='cursor-pointer' onClick={() => selectDropdown(i)}>
-											{selectedDropdown === i ? <IoIosArrowUp /> : <IoIosArrowDown />}
-										</span>
-									</div>
-									{selectedDropdown === i && (
-										<div className='mt-3 p-2 border-t text-gray-700'>
-											<div className='flex px-3 py-1 items-center justify-between'>
-												<span>პირველი 1 საათი</span>
-												<span className='text-green-500'>{counter.first_hours}₾</span>
-											</div>
-											<div className='flex px-3 py-1 items-center justify-between'>
-												<span>3 საათის განმავლობაში</span>
-												<span className='text-green-500'>{counter.during_3_hours}₾</span>
-											</div>
-											<div className='flex px-3 py-1 items-center justify-between'>
-												<span>4 საათის შემდეგ ღამის 11-მდე</span>
-												<span className='text-green-500'>{counter.until_23_oclock}₾</span>
-											</div>
-											<div className='flex px-3 py-1 items-center justify-between'>
-												<span>ღამის 11 დან დილის 9-მდე</span>
-												<span className='text-green-500'>{counter.until_9_oclock}₾</span>
-											</div>
-											<div className='flex px-3 py-1 items-center justify-between'>
-												<span>ყოველი 24 საათი</span>
-												<span className='text-green-500'>{counter.every_24_hours}₾</span>
-											</div>
-										</div>
-									)}
-								</div>
-							);
-						})}
-					</div>
-				</ModalBody>
-				<ModalFooter>
-					<button className='py-2 px-5 bg-green-500 hover:bg-green-400 active:bg-green-600 text-white rounded-md disabled:bg-green-400/[.6]' disabled={!selectedCounter} onClick={fitCounter}>
-						შენახვა
-					</button>
-				</ModalFooter>
-			</Modal>
-		</ModalContainer>
-	);
+  return (
+    <ModalContainer>
+      <Modal className="w-[36.75rem]">
+        <ModalHeader
+          title="მრიცხველის მორგება"
+          onHide={hideFitDevice}
+          className="flex justify-between items-center"
+        >
+          <h5 className="font-bold text-[1.375rem] text-text-gray-primary px-[0.625rem]">
+            მოწყობილობის დამატება
+          </h5>
+        </ModalHeader>
+        <ModalBody>
+          <div className="px-[0.625rem]">
+            <div className="text-[1rem] font-medium text-text-primary-blue my-[1.375rem]">
+              {selectedDevice.device_name}
+            </div>
+            {counters.map((counter, i) => {
+              let bgColor =
+                counter.counter_id === selectedCounter
+                  ? "bg-text-primary-blue"
+                  : "";
+              return (
+                <div key={i} className="rounded-lg shadow-custom">
+                  <div className="flex items-center justify-between pl-[0.625rem] py-[1.063rem] pr-6 ">
+                    <div className="flex items-center gap-x-3">
+                      <div
+                        className={`${bgColor} w-[1.313rem] h-[1.313rem] border-[0.2px] rounded-[3px] cursor-pointer flex items-center justify-center`}
+                        onClick={() =>
+                          selectCounter(counter.counter_id as number)
+                        }
+                      >
+                        {counter.counter_id === selectedCounter && (
+                          <FiCheck className="text-white" />
+                        )}
+                      </div>
+                      <span className="text-text-gray-primary">
+                        {counter.counter_name}
+                      </span>
+                    </div>
+                    <span
+                      className="cursor-pointer"
+                      onClick={() => selectDropdown(i)}
+                    >
+                      {selectedDropdown === i ? (
+                        <IoIosArrowUp />
+                      ) : (
+                        <IoIosArrowDown />
+                      )}
+                    </span>
+                  </div>
+                  {selectedDropdown === i && (
+                    <div className="m-[0.625rem] border-t-[1px] text-text-gray-primary flex flex-col gap-5 pt-[0.938rem] pb-[1.563rem]">
+                      <div className="flex px-3 items-center justify-between">
+                        <span>პირველი 1 საათი</span>
+                        <span className="text-button-green">
+                          {counter.first_hours}₾
+                        </span>
+                      </div>
+                      <div className="flex px-3 items-center justify-between">
+                        <span>3 საათის განმავლობაში</span>
+                        <span className="text-button-green">
+                          {counter.during_3_hours}₾
+                        </span>
+                      </div>
+                      <div className="flex px-3 items-center justify-between">
+                        <span>4 საათის შემდეგ ღამის 11-მდე</span>
+                        <span className="text-button-green">
+                          {counter.until_23_oclock}₾
+                        </span>
+                      </div>
+                      <div className="flex px-3 items-center justify-between">
+                        <span>ღამის 11 დან დილის 9-მდე</span>
+                        <span className="text-button-green">
+                          {counter.until_9_oclock}₾
+                        </span>
+                      </div>
+                      <div className="flex px-3 items-center justify-between">
+                        <span>ყოველი 24 საათი</span>
+                        <span className="text-button-green">
+                          {counter.every_24_hours}₾
+                        </span>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button
+            className="mt-[1.875rem] mx-auto block bg-button-green text-white rounded-md w-[9.625rem] h-[2.5rem] font-bold disabled:bg-green-400/[.6]"
+            disabled={!selectedCounter}
+            onClick={fitCounter}
+          >
+            შენახვა
+          </button>
+        </ModalFooter>
+      </Modal>
+    </ModalContainer>
+  );
 };
 
 export default FitCounter;
